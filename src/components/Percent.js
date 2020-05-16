a05a0c1554d4d873aa43ae40a09fe8436432c225
KaranRandhir <karanrandhir123@gmail.com>
2020-05-16 18:10:16 +0530

    Update Percent.js

--- src/components/Percent.js
+++ src/components/Percent.js
@@ -14,7 +14,7 @@ class Percent extends React.Component {
         <div>
         {this.state.visible?<Spring from ={{number:0}}
         to={{number:this.props.number}}
-        config={{duration:1800}}>
+        config={{duration:1400}}>
          {(props)=> props.number.toFixed()+"%"}
         </Spring>:"."}
         </div> 
