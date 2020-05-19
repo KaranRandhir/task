06500671deefa7c629e798a89b3e55f7af8b6398
KaranRandhir <karanrandhir123@gmail.com>
2020-05-19 13:09:48 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -111,7 +111,7 @@ class Home extends React.Component{
         
                         <div className="single  blue" >
                         <br/>
-                        <button className="search"style={{height:"4rem",fontSize:"1.5",fontWeight:"300",width:"180px",margin:"auto"}}className="ui blue big button">
+                        <button onClick={()=>alert("coming soon")} className="search"style={{height:"4rem",fontSize:"1.5",fontWeight:"300",width:"180px",margin:"auto"}}className="ui blue big button">
                         SEARCH
                         </button>
                         </div>
