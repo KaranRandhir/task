7633d8520590102dc516cb30ef0d0b657cd8e4ae
KaranRandhir <karanrandhir123@gmail.com>
2020-05-22 15:01:57 +0530

    logo added

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -1,17 +1,22 @@
 import React from "react"
 import "./nav1.css"
-
+import crane from "../assets/crane.png"
 import { BrowserRouter as Router,Route,Link } from "react-router-dom"
 
 const Nav1 = () => {
     return (
         <div class="ui borderless  menu">
         <div class="header item company">
+        <div style={{display:"flex"}}>
+        <img src={crane} className="crane"/>
+          <div style={{marginLeft:".5vw"}}>
           <div className="title">
           IndiaPort<br/>
           </div>
           <div className="tagline">
           Simplifying Boundaries
+          </div>
+          </div>
           </div>
         </div>
         <div className="menu right"> 
